69010e8b3d3462ec44cc4cca154e91c98261232d
Jed Parsons <jedp@me.com>
2012-12-07 22:45:42 -0800

    documentation an test for awsbox config creation

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ local hacking
 =============
 
 1. `git clone` this repo
-2. cd into that directory
+2. `cd` into that directory
 3. `npm install`
 4. Setup a database
 
@@ -19,7 +19,7 @@ local hacking
         mysql> create database dev_roomr
         mysql> quit
 
-        $ foreach schema (`ls -1 server/db/schema`)
+        $ foreach schema (`ls -1 server/db`)
         mysql -u root -ppassword dev_roomr < $schema
         end
 
@@ -30,3 +30,37 @@ local hacking
 
 6. `npm start`
 7. Visit http://localhost:9714
+
+awsbox hacking
+==============
+
+1. `git clone` this repo
+2. `cd` into that directory
+3. `npm install`
+4. Get your aws creds into your environment:
+
+    export AWS_ID=your_aws_id
+    export AWS_SECRET=your_aws_secret
+
+5. Deploy!
+
+    node_modules/.bin/awsbox create -n roomr
+
+6. Push code to start server:
+
+    git push roomr master
+
+Now hack and make branches and be happy.  When you want to test something, `git
+push` it to `roomr` and the server will be updated with your changes.
+
+Note that this will currently push all the schema updates in `server/db` on
+creation, but any subsequent updates will not have any effect with git pushes.
+You will have to implement them manually by ssh'ing to `app@your_ip`, invoking
+`mysql -u roomr`, and making the updates yourself.
+
+If that seems too laborious, just delete the awsbox instance and create another :)
+
+If you forget your awsbox ip address, you can get it from `git remote -v`.
+
+
+
--- scripts/make_config.js
+++ scripts/make_config.js
@@ -1,19 +1,41 @@
 #!/usr/bin/env node
 
+/**
+ * Take the config.js-dist file and write a config.js for awsbox
+ * 
+ * Since this does string replacement on the config.js-dist file, and depends on files
+ * being in certain directories, it is a bit fragile.  The scripts_make_config_vows.js
+ * test is there to make sure it doesn't fly apart.
+ */
+
 const
 crypto = require('crypto'),
 fs = require('fs'),
-path = require('path');
+path = require('path'),
+SERVER_ETC = '../server/etc';
 
-var secret = crypto.randomBytes(64).toString('base64');
-var config = fs.readFileSync(path.join(__dirname, '../server/etc', 'config.js-dist')).toString();
-var awsbox_config = path.join(__dirname, '../../config.json');
-var public_url = JSON.parse(fs.readFileSync(awsbox_config).toString()).public_url;
+function makeConfig(public_url) {
+  var secret = crypto.randomBytes(64).toString('base64');
+  var config = fs.readFileSync(path.join(__dirname, SERVER_ETC, 'config.js-dist')).toString();
 
-config = config.replace(/^exports.audience = .*$/m, 'exports.audience = "'+public_url+'";');
-config = config.replace("CHANGE ME TO SOMETHING RANDOM", secret);
-config = config.replace('"user"', '"roomr"');
-config = config.replace('"password"', 'null');
-config = config.replace('"dev_roomr"', '"roomr"');
+  config = config.replace(/^exports.audience = .*$/m, 'exports.audience = "'+public_url+'";');
+  config = config.replace("CHANGE ME TO SOMETHING RANDOM", secret);
+  config = config.replace('"user"', '"roomr"');
+  config = config.replace('"password"', 'null');
+  config = config.replace('"dev_roomr"', '"roomr"');
 
-fs.writeFileSync(path.join(__dirname, '../server/etc', 'config.js'), config, 'ascii');
+  return config;
+}
+
+if (module.parent) {
+  module.exports = makeConfig
+} else {
+  // this will exist on an awsbox deployment
+  var awsbox_config = path.join(__dirname, '../../config.json');
+  if (fs.existsSync(awsbox_config)) {
+    var public_url = JSON.parse(fs.readFileSync(awsbox_config).toString()).public_url;
+    fs.writeFileSync(path.join(__dirname, SERVER_ETC, 'config.js'), makeConfig(public_url), 'ascii');
+  } else {
+    console.log("Not found: " + awsbox_json);
+  }
+}
